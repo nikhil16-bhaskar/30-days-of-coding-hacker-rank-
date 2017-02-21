4250e06e6431b2e83f127b8ecfadbc2749c016a7
nikhil bhaskar <perfectnikhil1697@gmail.com>
2017-02-21 14:55:45 +0530

    day 1 of 30 day of coding hacker rank

---
+++ day 1.cpp
@@ -0,0 +1,17 @@
+   #include<iostream>
+   #include<string.h>
+   using namespace std;
+   int main()
+   {
+   string s;
+   float number1;
+   float number2;
+   cin>>number1>>number2;
+   float sum;
+   sum=number1+number2;
+   cout<<sum<<endl;
+   sum=number2+number2;
+   cout<<"enter string"<<endl;
+   cin>>s;
+   cout<<s<<" is the best place to learn and practice coding!";
+   }
